39ae56c672673f49f7127bcf9844426d9388e15a
kiyo-8jo <csta13054@gmail.com>
2025-04-03 18:56:33 +0900

    buildError解消のためfetchのurlを変更

--- src/app/components/home/newInfo/NewFiveCardsContainer/NewFiveCardsContainer.tsx
+++ src/app/components/home/newInfo/NewFiveCardsContainer/NewFiveCardsContainer.tsx
@@ -4,7 +4,9 @@ import Card from "@/app/components/common/card/Card";
 
 const NewFiveCardsContainer = async () => {
   // 全てのデータを新着順で取得
-  const res = await fetch("http://localhost:3000/api/getAllData");
+  const res = await fetch(
+    "https://real-estate-git-main-kiyo-8jos-projects.vercel.app/api/getAllData"
+  );
   const realEstates = await res.json();
   // 新着の5件のみ表示
   const filteredRealEstates = realEstates.slice(0, 5);
