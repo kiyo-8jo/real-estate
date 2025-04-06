2fdc3f1b3abaf83f21a4ae9b4b2cefa3c554adab
kiyo-8jo <csta13054@gmail.com>
2025-04-06 15:06:10 +0900

    resendのinstallとEmailを送るapiの作成、ドメインの取得

---
+++ src/app/api/sendEmail/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+import Email from "../../../../emails/Email";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req:NextRequest) {
+  const { name, tel, email, inquiry } = await req.json();
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: process.env.MY_ADDRESS!,
+      subject: "Island Real Estate",
+      react: Email({ name, tel, email, inquiry }),
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
