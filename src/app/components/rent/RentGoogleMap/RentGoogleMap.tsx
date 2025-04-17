959e4af0661b67bf25839cd704de5d8ae22f341d
kiyo-8jo <csta13054@gmail.com>
2025-04-17 12:43:10 +0900

    googlemapが表示されていなかったので修正

--- src/app/components/rent/RentGoogleMap/RentGoogleMap.tsx
+++ src/app/components/rent/RentGoogleMap/RentGoogleMap.tsx
@@ -1,22 +1,22 @@
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: "400px",
-  height: "400px",
+  width: "100%",
+  height: "500px",
 };
 
 const center = {
-  lat: 35.69575,
-  lng: 139.77521,
+  lat: 33.110687,
+  lng: 139.791294,
 };
 
 const RentGoogleMap = () => {
-  const GOOGLE_MAP_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
+  const GOOGLE_MAP_API_KEY = process.env.NEXT_PUBLIC_MY_GOOGLE_MAP_API_KEY!;
   return (
-    <LoadScript googleMapsApiKey={`${GOOGLE_MAP_API_KEY}`}>
+    <LoadScript googleMapsApiKey={GOOGLE_MAP_API_KEY}>
       <GoogleMap
         center={center}
-        zoom={17}
+        zoom={12}
         mapContainerStyle={containerStyle}
       ></GoogleMap>
     </LoadScript>
