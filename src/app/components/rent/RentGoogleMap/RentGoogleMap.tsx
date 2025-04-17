f7f772216ce2b55e758b7bd03acc6aa336cec7ad
kiyo-8jo <csta13054@gmail.com>
2025-04-17 14:37:11 +0900

    rentpageにgooglemapを表示

--- src/app/components/rent/RentGoogleMap/RentGoogleMap.tsx
+++ src/app/components/rent/RentGoogleMap/RentGoogleMap.tsx
@@ -1,25 +1,80 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  InfoWindow,
+  LoadScript,
+  MarkerF,
+} from "@react-google-maps/api";
+import Link from "next/link";
+import { useState } from "react";
+import styles from "./RentGoogleMap.module.css";
 
+// 地図全体のcss
 const containerStyle = {
   width: "100%",
-  height: "500px",
+  height: "550px",
 };
 
+// 地図の緯度経度
 const center = {
   lat: 33.110687,
   lng: 139.791294,
 };
 
+// マーカーの緯度経度
+const marker1 = {
+  lat: 33.147414,
+  lng: 139.773632,
+};
+const marker2 = {
+  lat: 33.094291,
+  lng: 139.818951,
+};
+
+// 吹き出しのcss
+const divStyle = {
+  background: "white",
+  fontSize: 8,
+};
+
 const RentGoogleMap = () => {
   const GOOGLE_MAP_API_KEY = process.env.NEXT_PUBLIC_MY_GOOGLE_MAP_API_KEY!;
+  const [size, setSize] = useState<undefined | google.maps.Size>(undefined);
+  const infoWindowOptions = {
+    pixelOffset: size,
+  };
+  const createOffsetSize = () => {
+    return setSize(new window.google.maps.Size(0, -45));
+  };
   return (
-    <LoadScript googleMapsApiKey={GOOGLE_MAP_API_KEY}>
-      <GoogleMap
-        center={center}
-        zoom={12}
-        mapContainerStyle={containerStyle}
-      ></GoogleMap>
-    </LoadScript>
+    <div className={styles.wrapper}>
+      <LoadScript googleMapsApiKey={GOOGLE_MAP_API_KEY}>
+        <GoogleMap
+          center={center}
+          zoom={12}
+          mapContainerStyle={containerStyle}
+          onLoad={() => createOffsetSize()}
+        >
+          {/* マーカー */}
+          <MarkerF position={marker1} />
+          <MarkerF position={marker2} />
+          {/* 吹き出し */}
+          <InfoWindow position={marker1} options={infoWindowOptions}>
+            <div style={divStyle}>
+              <Link href="/">
+                <h1>賃貸物件1</h1>
+              </Link>
+            </div>
+          </InfoWindow>
+          <InfoWindow position={marker2} options={infoWindowOptions}>
+            <div style={divStyle}>
+              <Link href="/">
+                <h1>賃貸物件2</h1>
+              </Link>
+            </div>
+          </InfoWindow>
+        </GoogleMap>
+      </LoadScript>
+    </div>
   );
 };
 
