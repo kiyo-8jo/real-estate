f616fbda3d8c05146b8fc6d9913aa310c04260f9
kiyo-8jo <csta13054@gmail.com>
2025-04-21 16:41:19 +0900

    detailpageに表示している物件に対応するimgをstorageより取得し表示

--- src/app/components/detail/DetailImgs/DetailImgs.tsx
+++ src/app/components/detail/DetailImgs/DetailImgs.tsx
@@ -6,13 +6,27 @@ interface DetailImgsProps {
   id: number;
 }
 
-const DetailImgs = (props: DetailImgsProps) => {
-  console.log(props.id);
-  const { data } = supabase.storage.from("images").getPublicUrl("1-1.png");
-  const imgUrl = data.publicUrl;
+const DetailImgs = async (props: DetailImgsProps) => {
+  // 表示している物件に該当するimgをstorageから取得
+  const { data } = await supabase.storage.from("images").list(`${props.id}`);
+  const dataLength = data!.length;
+
+  //imgのurlを格納する配列
+  const urlAry = [];
+
+  for (let i = 1; i <= dataLength; i++) {
+    const { data } = supabase.storage
+      .from("images")
+      .getPublicUrl(`${props.id}/${i}.png`);
+    const imgUrl = data.publicUrl;
+    urlAry.push(imgUrl);
+  }
+
   return (
     <div>
-      <Image src={imgUrl} alt={imgUrl} width={500} height={400} />
+      {urlAry.map((url) => {
+        return <Image src={url} alt={url} width={500} height={400} key={url} />;
+      })}
     </div>
   );
 };
