c5fb56bd45bba7900b024efb656d6c52230883bc
kiyo-8jo <csta13054@gmail.com>
2025-04-21 19:38:34 +0900

    swiperのデザイン選定

--- src/app/components/detail/DetailImgs/DetailImgs.tsx
+++ src/app/components/detail/DetailImgs/DetailImgs.tsx
@@ -3,13 +3,28 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+import { Pagination, Navigation } from "swiper/modules";
+
 interface DetailImgsProps {
   urlAry: string[];
 }
 
 const DetailImgs = (props: DetailImgsProps) => {
   return (
-    <Swiper>
+    <Swiper
+      pagination={{
+        type: "fraction",
+      }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className="mySwiper"
+    >
       {props.urlAry.map((url) => (
         <SwiperSlide key={url}>
           <Image src={url} alt={url} width={500} height={400} />
