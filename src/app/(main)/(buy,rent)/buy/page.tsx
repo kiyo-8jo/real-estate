6951a762b5a852578b45fb6c4ec457e30fa25a41
kiyo-8jo <csta13054@gmail.com>
2025-04-25 14:43:35 +0900

    builderror解消するため

--- src/app/(main)/(buy,rent)/buy/page.tsx
+++ src/app/(main)/(buy,rent)/buy/page.tsx
@@ -3,8 +3,8 @@ import styles from "./page.module.css";
 import SelectButtons from "@/app/components/common/filterAndResetButtons/FilterAndResetButtons";
 import { Suspense } from "react";
 import Loading from "@/app/components/common/loading/Loading";
-import BuyGoogleMap from "@/app/components/buy/buyGoogleMap/BuyGoogleMap";
 import AllBuyCardsContainer from "@/app/components/buy/allBuyCardsContainer/AllBuyCardsContainer";
+import BuyGoogleMap from "@/app/components/buy/BuyGoogleMap/BuyGoogleMap";
 
 const BuyPage = async ({
   searchParams,
