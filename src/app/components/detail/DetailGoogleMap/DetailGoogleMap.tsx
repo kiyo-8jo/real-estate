c8fe63b24460d52d7012df9842e583e9030ceff1
kiyo-8jo <csta13054@gmail.com>
2025-04-26 20:32:24 +0900

    detailpageのgooglemapのwarningを解消

--- src/app/components/detail/DetailGoogleMap/DetailGoogleMap.tsx
+++ src/app/components/detail/DetailGoogleMap/DetailGoogleMap.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Libraries, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 import styles from "./DetailGoogleMap.module.css";
 
 interface LatAndLng {
@@ -8,13 +8,15 @@ interface LatAndLng {
   mapLng: number;
 }
 
+const libraries = ["visualization"] as Libraries;
+
 const DetailGoogleMap = (props: LatAndLng) => {
   const GOOGLE_MAP_API_KEY = process.env.NEXT_PUBLIC_MY_GOOGLE_MAP_API_KEY!;
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: GOOGLE_MAP_API_KEY,
-    libraries: ["geometry", "drawing"],
+    libraries,
   });
 
   // 地図全体のcss
