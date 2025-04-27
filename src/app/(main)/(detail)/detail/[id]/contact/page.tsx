811d9c23e13a423e45cd76f1035a0914f2e3f3a2
kiyo-8jo <csta13054@gmail.com>
2025-04-27 15:01:21 +0900

    detailcontactpageをssgに、修正

--- src/app/(main)/(detail)/detail/[id]/contact/page.tsx
+++ src/app/(main)/(detail)/detail/[id]/contact/page.tsx
@@ -1,6 +1,9 @@
 import styles from "./page.module.css";
 import DetailContactForm from "@/app/components/detail/DetailContactForm/DetailContactForm";
 import DetailContactTitle from "@/app/components/detail/DetailContactTitle/DetailContactTitle";
+import { RealEstateDataType } from "@/app/types/types";
+
+export const dynamic = "force-static";
 
 const DetailContactPage = async ({
   params,
@@ -8,10 +11,13 @@ const DetailContactPage = async ({
   params: Promise<{ id: number }>;
 }) => {
   const { id } = await params;
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${API_URL}/api/getDetailData/${id}`);
+  const realEstate: RealEstateDataType = await res.json();
 
   return (
     <div className={styles.wrapper}>
-      <DetailContactTitle id={id} />
+      <DetailContactTitle realEstate={realEstate} />
       <DetailContactForm id={id} />
     </div>
   );
--- src/app/components/detail/DetailContactTitle/DetailContactTitle.tsx
+++ src/app/components/detail/DetailContactTitle/DetailContactTitle.tsx
@@ -2,17 +2,13 @@ import { RealEstateDataType } from "@/app/types/types";
 import styles from "./DetailContactTitle.module.css";
 
 interface DetailContactTitleProps {
-  id: number;
+  realEstate: RealEstateDataType;
 }
 
-export const dynamic = "force-static";
-
-const DetailContactTitle = async (props: DetailContactTitleProps) => {
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  const res = await fetch(`${API_URL}/api/getDetailData/${props.id}`);
-  const realEstate: RealEstateDataType = await res.json();
-
-  return <h2 className={styles.title}>{`${realEstate.name}の問い合わせ`}</h2>;
+const DetailContactTitle = (props: DetailContactTitleProps) => {
+  return (
+    <h2 className={styles.title}>{`${props.realEstate.name}の問い合わせ`}</h2>
+  );
 };
 
 export default DetailContactTitle;
