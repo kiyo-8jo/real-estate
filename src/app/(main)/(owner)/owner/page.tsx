8ff22f4247fdc103af6243675d27b5c1fa15fd59
kiyo-8jo <csta13054@gmail.com>
2025-04-28 18:55:04 +0900

    ownerpageのformでserverActionを試してみる

--- src/app/(main)/(owner)/owner/page.tsx
+++ src/app/(main)/(owner)/owner/page.tsx
@@ -5,70 +5,23 @@ import styles from "./page.module.css";
 import { useState } from "react";
 import Document from "@/app/components/common/document/Document";
 import { useRouter } from "next/navigation";
+import { sendOwnerEmailAction } from "@/app/hooks/email/sendOwnerEmailAction";
 
 const OwnerPage = () => {
   const router = useRouter();
   // documentを表示させるかどうかのstate
   const [display, setDisplay] = useState<boolean>(false);
-  // checkboxにcheckされているかどうかのstate
-  const [checked, setChecked] = useState<boolean>(false);
-
-  // form入力内容
-  const [name, setName] = useState<string>("");
-  const [tel, setTel] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [inquiry, setInquiry] = useState<string>("");
-
-  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(getSanitizedValue(e.target.value));
-  };
-  const handleTel = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTel(getSanitizedValue(e.target.value));
-  };
-  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(getSanitizedValue(e.target.value));
-  };
-  const handleInquiry = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInquiry(getSanitizedValue(e.target.value));
-  };
-  const handleChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(e.target.checked);
-  };
-
-  // サニタイズ用関数
-  const getSanitizedValue = (inputValue: string) => {
-    return String(inputValue)
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/'/g, "&apos;")
-      .replace(/"/g, "&quot;");
-  };
-
-  // formのsubmit
-  const handleSubmit = async (e: React.FormEvent) => {
-    const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-    e.preventDefault();
-
-    try {
-      await fetch(`${API_URL}/api/sendEmail/ownerContact`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, tel, email, inquiry }),
-      });
-    } catch (error) {
-      alert(error);
-    }
-    router.push("/");
-    alert("メールを送信しました");
-  };
 
   return (
     <div className={styles.wrapper}>
       <Title title="オーナー様用お問い合わせ" />
-      <form onSubmit={handleSubmit}>
+      <form
+        action={async (formData) => {
+          await sendOwnerEmailAction(formData);
+          alert('メールを送信しました')
+          router.push('/')
+        }}
+      >
         {/* 名前 */}
         <div className={styles.form_part_container}>
           <div className={styles.label_container}>
@@ -80,8 +33,7 @@ const OwnerPage = () => {
             id="name"
             placeholder="田中太郎"
             autoComplete="name"
-            value={name}
-            onChange={handleName}
+            name="name"
             required
           />
         </div>
@@ -95,8 +47,7 @@ const OwnerPage = () => {
             id="tel"
             placeholder="000-0000-0000"
             autoComplete="tel"
-            value={tel}
-            onChange={handleTel}
+            name="tel"
             required
           />
         </div>
@@ -111,8 +62,7 @@ const OwnerPage = () => {
             id="email"
             placeholder="info@example.com"
             autoComplete="email"
-            value={email}
-            onChange={handleEmail}
+            name="email"
             required
           />
         </div>
@@ -124,8 +74,7 @@ const OwnerPage = () => {
           <textarea
             id="inquiry"
             placeholder="お問い合わせ内容を入力してください"
-            value={inquiry}
-            onChange={handleInquiry}
+            name="inquiry"
             required
           ></textarea>
         </div>
@@ -137,13 +86,7 @@ const OwnerPage = () => {
                 クリックしてポリシーを確認して下さい
               </span>
               <div className={styles.input_container}>
-                <input
-                  type="checkbox"
-                  id="checkbox"
-                  checked={checked}
-                  onChange={handleChecked}
-                  required
-                />
+                <input type="checkbox" id="checkbox" required />
                 <label htmlFor="checkbox">ポリシーを確認し、同意しました</label>
               </div>
               {display && <Document />}
---
+++ src/app/hooks/email/sendOwnerEmailAction.ts
@@ -0,0 +1,31 @@
+"use server";
+import { Resend } from "resend";
+import Email from "../../../../emails/OwnerContactEmail";
+
+export const sendOwnerEmailAction = async (formData: FormData) => {
+  // サニタイズ用関数
+  const getSanitizedValue = (inputValue: FormDataEntryValue | null) => {
+    return String(inputValue)
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/'/g, "&apos;")
+      .replace(/"/g, "&quot;");
+  };
+  const name = getSanitizedValue(formData.get("name"));
+  const tel = getSanitizedValue(formData.get("tel"));
+  const email = getSanitizedValue(formData.get("email"));
+  const inquiry = getSanitizedValue(formData.get("name"));
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: process.env.MY_ADDRESS!,
+      subject: "Island Real Estate",
+      react: Email({ name, tel, email, inquiry }),
+    });
+  } catch (error) {
+    return console.log(error);
+  }
+};
