612ecafaa7b36a2052e7294b8ed52d81f4798e4a
kiyo-8jo <csta13054@gmail.com>
2026-01-08 12:14:03 +0900

    ビルドエラー解消

--- src/app/(main)/(buy,rent)/buy/layout.tsx
+++ src/app/(main)/(buy,rent)/buy/layout.tsx
@@ -2,9 +2,9 @@ import BuyGoogleMap from "@/app/components/buy/BuyGoogleMap/BuyGoogleMap";
 import FilterAndResetButtons from "@/app/components/common/filterAndResetButtons/FilterAndResetButtons";
 import Title from "@/app/components/common/title/Title";
 import styles from "./layout.module.css";
-import { ReactElement, Suspense } from "react";
+import { ReactNode, Suspense } from "react";
 
-const Layout = ({ children }: { children: ReactElement }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className={styles.wrapper}>
       <Title title={"買う / Buy"} />
--- src/app/(main)/(buy,rent)/rent/layout.tsx
+++ src/app/(main)/(buy,rent)/rent/layout.tsx
@@ -1,10 +1,10 @@
 import FilterAndResetButtons from "@/app/components/common/filterAndResetButtons/FilterAndResetButtons";
 import Title from "@/app/components/common/title/Title";
 import styles from "./layout.module.css";
-import React, { ReactElement, Suspense } from "react";
+import React, { ReactNode, Suspense } from "react";
 import RentGoogleMap from "@/app/components/rent/RentGoogleMap/RentGoogleMap";
 
-const Layout = ({ children }: { children: ReactElement }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className={styles.wrapper}>
       <Title title={"借りる / Rent"} />
