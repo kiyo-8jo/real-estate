7a8dde4f41cf281b37f386dbb33577ac8634ef4e
kiyo-8jo <csta13054@gmail.com>
2026-01-08 11:05:35 +0900

    Revert "メールの送信元アドレス変更"
    This reverts commit 288e6267c2d7ad6488c512eeb4cf632283b72fbb.

--- src/app/api/sendEmail/detailContact/route.ts
+++ src/app/api/sendEmail/detailContact/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: NextRequest) {
     const realEstate: RealEstateDataType = await res.json();
 
     const data = await resend.emails.send({
-      from: "onboarding@8jo-real-estate.jp",
+      from: "onboarding@resend.dev",
       to: process.env.MY_ADDRESS!,
       subject: "Island Real Estate",
       react: Email({
