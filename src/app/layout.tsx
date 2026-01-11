46967c9cd6e615a7d3f9704e90d96881c2f6ed41
kiyo-8jo <csta13054@gmail.com>
2026-01-11 12:56:27 +0900

    フォントを変えてみる

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/app/layouts/header/Header";
 import Footer from "@/app/layouts/footer/Footer";
+import { Geist } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-  preload: true,
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-  preload: true,
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+//   preload: true,
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+//   preload: true,
+// });
+const geist = Geist({
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -29,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={`${geistSans.variable} ${geistMono.variable} `}>
+      <body className={geist.className}>
         <Header />
         {children}
         <Footer />
