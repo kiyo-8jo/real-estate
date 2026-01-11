2c701bafeb23d5bef60eb8c9a950b41625bbc786
kiyo-8jo <csta13054@gmail.com>
2026-01-11 12:30:40 +0900

    warningが解消されないのでフォントが原因かもしれないと思い修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,13 +8,13 @@ const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-  preload: false,
+  preload: true,
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-  preload: false,
+  preload: true,
 });
 
 export const metadata: Metadata = {
